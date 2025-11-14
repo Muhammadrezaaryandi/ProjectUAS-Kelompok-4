684e2fa3a9fdb777616f9f136e367b378a862f4c
ejaahakim <rezanurhkm@mhs.usk.ac.id>
2025-11-14 23:26:50 +0700

    menampilkan daftar alat

--- Sistem-peminjaman-alat-lab/admin.c
+++ Sistem-peminjaman-alat-lab/admin.c
@@ -15,3 +15,20 @@ int hitung_baris_file(char *namaFile)
 
 // Bagian ini berfungsi untuk membaca semua data alat dari file "alat.txt"
 void tampilkan_alat()
+{
+    FILE *fp = fopen("alat.txt", "r");
+    if(!fp)
+    {
+        printf("Belum ada data alat.\n");
+        return;
+    }
+
+    Alat alat;
+    printf("\n=== DAFTAR ALAT LAB ===\n");
+    printf("%-5s %-20s %-15s %-15s %-7s %s\n", 
+           "ID", "Nama", "Merek", "Model", "Tahun", "Jumlah");
+    printf("========================================================================\n");
+
+    while (fscanf(fp, "%u,%[^,],%[^,],%[^,],%u,%u\n", 
+        &alat.id, alat.nama, alat.merek, alat.model, &alat.tahun, &alat.jumlah) == 6)
+        {
