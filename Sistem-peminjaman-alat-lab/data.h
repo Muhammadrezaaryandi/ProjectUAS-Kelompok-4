e0be15af265d6be43631381d477bcabd41f4092f
ihsansyahni <ihsansyahn@mhs.usk.ac.id>
2025-11-14 21:47:41 +0700

    Menambahkan definisi struct untuk data alat (id, nama, merek)

--- Sistem-peminjaman-alat-lab/data.h
+++ Sistem-peminjaman-alat-lab/data.h
@@ -5,3 +5,10 @@
 #include <stdlib.h>
 #include <string.h>
 // Berbagai header
+// =============================
+// STRUCT DATA
+// =============================
+typedef struct {
+    unsigned int id;
+    char nama[50];
+    char merek[50];
