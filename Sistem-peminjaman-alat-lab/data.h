2108c64e415c29d5d9425c0bcc2351e2ef636070
ihsansyahni <ihsansyahn@mhs.usk.ac.id>
2025-11-14 21:50:41 +0700

    Menambahkan struct Alat, Akun, dan Peminjaman

--- Sistem-peminjaman-alat-lab/data.h
+++ Sistem-peminjaman-alat-lab/data.h
@@ -12,3 +12,20 @@ typedef struct {
     unsigned int id;
     char nama[50];
     char merek[50];
+    char model[50];
+    unsigned int tahun;
+    unsigned int jumlah;
+} Alat;
+
+typedef struct {
+    char username[50];
+    char password[50];
+    char role[10]; // "admin" atau "user"
+} Akun;
+
+typedef struct {
+    char username[50];
+    unsigned int idAlat;
+    char namaAlat[50];
+    unsigned int jumlahPinjam;
+} Peminjaman;
