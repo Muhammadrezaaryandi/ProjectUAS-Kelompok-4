19ecd1ddebd5d6c6f3fd487817a16942d5d431c5
ihsansyahni <ihsansyahn@mhs.usk.ac.id>
2025-11-16 04:23:10 +0700

    Menambahkan fungsi garis dan struktur dasar fungsi main

---
+++ Sistem-peminjaman-alat-lab/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h> 
+#include "data.h"
+
+void garis() {
+    printf("========================================================================\n");
+}
+
+// Fungsi utama program
+int main(int argc, char *argv[]) {
