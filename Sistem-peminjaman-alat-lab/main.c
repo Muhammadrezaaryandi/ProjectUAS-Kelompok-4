b81d25c230290b706deb408abc914f6d702218ce
ihsansyahni <ihsansyahn@mhs.usk.ac.id>
2025-11-16 17:42:19 +0700

    Menambahkan validasi argumen dan proses login pada program utama

--- Sistem-peminjaman-alat-lab/main.c
+++ Sistem-peminjaman-alat-lab/main.c
@@ -8,3 +8,22 @@ void garis() {
 
 // Fungsi utama program
 int main(int argc, char *argv[]) {
+
+        // Validasi jumlah argumen command line
+    if (argc < 3) 
+    {
+        printf("Format: %s <username> <password>\n", argv[0]);
+        return 1;
+    }
+
+    // Variabel untuk menyimpan role user
+    char role[10];
+    
+    // Proses login dengan memanggil fungsi login dari alat.h
+    if (!login(argv[1], argv[2], role)) 
+    {
+        printf("[!] Login gagal. Username atau password salah.\n");
+        return 0;
+    }
+
+    // Tampilan setelah login berhasil
